0646755fcb913fafb662e86b25f8801ecaa48f24
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2022-09-22 15:02:19 -0500

    rename DeviceManifest -> ManifestApp

--- DjangoManifest/settings.py
+++ DjangoManifest/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'DeviceManifest.apps.DevicemanifestConfig',
+    'ManifestApp.apps.ManifestappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ManifestApp/apps.py
+++ ManifestApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DevicemanifestConfig(AppConfig):
+class ManifestappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DeviceManifest'
+    name = 'ManifestApp'
