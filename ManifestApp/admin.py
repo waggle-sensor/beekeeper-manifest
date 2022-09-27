805dd52e22c318cc91e30f753067a307a53787b5
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2022-09-27 13:54:06 -0500

    readme draft & data model inline

--- ManifestApp/admin.py
+++ ManifestApp/admin.py
@@ -2,9 +2,33 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(NodeData)
-admin.site.register(Compute)
-admin.site.register(Sensor)
-admin.site.register(Resource)
-admin.site.register(Tag)
-admin.site.register(Label)
+# admin.site.register(Compute)
+# admin.site.register(Sensor)
+# admin.site.register(Resource)
+# admin.site.register(Tag)
+# admin.site.register(Label)
+
+
+class ComputeInline(admin.StackedInline):
+    model = Compute
+
+class SensorInline(admin.StackedInline):
+    model = Sensor
+
+class ResourceInline(admin.StackedInline):
+    model = Resource
+
+# class TagInline(admin.StackedInline):
+#     model = Tag
+
+# class LabelInline(admin.StackedInline):
+#     model = Label
+
+class NodeMetaData(admin.ModelAdmin):
+    inlines = [
+        ComputeInline,
+        SensorInline,
+        ResourceInline,
+    ]
+
+admin.site.register(NodeData, NodeMetaData)
--- ManifestApp/models.py
+++ ManifestApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class NodeData(models.Model):
     VSN = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
-    # tags = models.CharField(max_length=30)
+    tags = models.ManyToManyField("Tag")
     gps_lat = models.FloatField()
     gps_lan = models.FloatField()
 
