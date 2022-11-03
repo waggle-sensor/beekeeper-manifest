20196194781ea064b786e76d16d877909438e9e5
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2022-11-03 14:39:50 -0500

    Change over url filter function

--- api/ManifestApp/views.py
+++ api/ManifestApp/views.py
@@ -1,19 +1,20 @@
 from django.contrib.auth.models import *
 from .models import *
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from .api.serializers import NodeSerializer
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('SAGE Beekeeper-Manifest')
 
-class NodeList(ListCreateAPIView):
 
+class NodeList(ListCreateAPIView):
     queryset = NodeData.objects.all().order_by("VSN")
     serializer_class = NodeSerializer
 
-    def get_queryset(self):
-        queryset = NodeData.objects.all().order_by("VSN")
-        if 'VSN' in self.request.GET:
-            return queryset.filter(VSN=self.request.GET['VSN'])
-        return queryset
+
+class NodeFilterList(RetrieveAPIView):
+
+    queryset = NodeData.objects.all().order_by("VSN")
+    serializer_class = NodeSerializer
+    lookup_field = "VSN"
