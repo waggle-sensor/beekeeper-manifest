5c4e2812c7e48983c0b075af519ef69ffecd145e
Ismael Perez <33365058+iperezx@users.noreply.github.com>
2022-11-21 13:49:14 -0800

    SAGE-1551: Add unit tests (#16)
    * delete unused code in tag model, add test script
    
    * change a variable in test.py
    
    * add gps fields to serializers.py, add test on api
    
    * assert api columns equal NodeData attributes
    
    * change VSN -> vsn
    
    Co-authored-by: SammiH2022 <96802164+SammiH2022@users.noreply.github.com>

--- api/ManifestApp/models.py
+++ api/ManifestApp/models.py
@@ -9,7 +9,7 @@ from django.contrib.gis.db import models as geo_models
 # NodeData
 class NodeData(models.Model):
 
-    VSN = models.CharField(max_length=30, unique="True")
+    vsn = models.CharField(max_length=30, unique="True")
     name = models.CharField(max_length=30)
     tags = models.ManyToManyField("Tag")
     computes = models.ManyToManyField("Hardware", through="Compute", related_name="computes")
@@ -19,7 +19,7 @@ class NodeData(models.Model):
 
 
     def __str__(self):
-         return self.VSN
+         return self.vsn
 
 # Hardware
 class Hardware(models.Model):
@@ -90,14 +90,8 @@ class Resource(models.Model):
         return self.name
 
 # Tag
-class TagManager(models.Manager):
-    def get_by_natural_key(self, tag):
-        return self.get(tag=tag)
-
 class Tag(models.Model):
     tag = models.CharField(max_length=30, unique="True")
-
-    objects = TagManager()
 
     def __str__(self):
         return self.tag
--- api/ManifestApp/tests.py
+++ api/ManifestApp/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import *
+import json
 
-# Create your tests here.
+class HomepageTest(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/ManifestApp/")
+        self.assertEqual(response.status_code, 200)
+
+class NodeTest(TestCase):
+    def setUp(self):
+        self.Node = NodeData.objects.create(vsn="A", name="A_name", gps_lat=50.00, gps_lan=50.00)
+
+        tag1 = Tag.objects.create(tag="t1")
+        self.Node.tags.set([tag1.pk])
+
+        compute1 = Hardware.objects.create(hardware="h1")
+        self.Node.computes.set([compute1.pk])
+
+        self.response = self.client.get("/ManifestApp/api/nodes/")
+
+    def test_node_creation(self):
+        N = self.Node
+        self.assertTrue(isinstance(N, NodeData))
+
+    def test_node_has_a_tag(self):
+        N = self.Node
+        self.assertEqual(N.tags.count(), 1)
+
+    def test_node_has_a_compute(self):
+        N = self.Node
+        self.assertEqual(N.computes.count(), 1)
+
+    def test_contains_expected_fields(self):
+        data = json.loads(self.response.content)
+
+        self.assertEqual(data[0].keys(), set(['vsn', 'name', 'resources', 'sensors', 'gps_lat', 'gps_lan', 'tags', 'computes']))
